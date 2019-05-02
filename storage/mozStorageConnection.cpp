9c7e57422b57892e04ea665dd8b396f18133ab3d
softwaretechone <50147726+softwaretechone@users.noreply.github.com>
2019-05-02 12:40:20 -0400

    Deleted Telemetry.h from #include directives and some telemetry

--- storage/mozStorageConnection.cpp
+++ storage/mozStorageConnection.cpp
@@ -13,7 +13,6 @@
 #include "nsThreadUtils.h"
 #include "nsIFile.h"
 #include "nsIFileURL.h"
-#include "mozilla/Telemetry.h"
 #include "mozilla/Mutex.h"
 #include "mozilla/CondVar.h"
 #include "mozilla/Attributes.h"
@@ -708,20 +707,6 @@ Connection::initializeInternal()
   if (mFileURL) {
     const char* dbPath = ::sqlite3_db_filename(mDBConn, "main");
     MOZ_ASSERT(dbPath);
-
-    const char* telemetryFilename =
-      ::sqlite3_uri_parameter(dbPath, "telemetryFilename");
-    if (telemetryFilename) {
-      if (NS_WARN_IF(*telemetryFilename == '\0')) {
-        return NS_ERROR_INVALID_ARG;
-      }
-      mTelemetryFilename = telemetryFilename;
-    }
-  }
-
-  if (mTelemetryFilename.IsEmpty()) {
-    mTelemetryFilename = getFilename();
-    MOZ_ASSERT(!mTelemetryFilename.IsEmpty());
   }
 
   // Properly wrap the database handle's mutex.
@@ -733,9 +718,6 @@ Connection::initializeInternal()
     ::sqlite3_trace_v2(mDBConn,
                        SQLITE_TRACE_STMT | SQLITE_TRACE_PROFILE,
                        tracefunc, this);
-
-    MOZ_LOG(gStorageLog, LogLevel::Debug, ("Opening connection to '%s' (%p)",
-                                        mTelemetryFilename.get(), this));
   }
 
   int64_t pageSize = Service::getDefaultPageSize();
@@ -1093,17 +1075,6 @@ Connection::stepStatement(sqlite3 *aNativeConnection, sqlite3_stmt *aStatement)
     ::sqlite3_reset(aStatement);
   }
 
-  // Report very slow SQL statements to Telemetry
-  TimeDuration duration = TimeStamp::Now() - startTime;
-  const uint32_t threshold =
-    NS_IsMainThread() ? Telemetry::kSlowSQLThresholdForMainThread
-                      : Telemetry::kSlowSQLThresholdForHelperThreads;
-  if (duration.ToMilliseconds() >= threshold) {
-    nsDependentCString statementString(::sqlite3_sql(aStatement));
-    Telemetry::RecordSlowSQLStatement(statementString, mTelemetryFilename,
-                                      duration.ToMilliseconds());
-  }
-
   (void)::sqlite3_extended_result_codes(aNativeConnection, 0);
   // Drop off the extended result bits of the result code.
   return srv & 0xFF;
@@ -1175,20 +1146,8 @@ Connection::executeSql(sqlite3 *aNativeConnection, const char *aSqlString)
   if (isClosed())
     return SQLITE_MISUSE;
 
-  TimeStamp startTime = TimeStamp::Now();
   int srv = ::sqlite3_exec(aNativeConnection, aSqlString, nullptr, nullptr,
                            nullptr);
-
-  // Report very slow SQL statements to Telemetry
-  TimeDuration duration = TimeStamp::Now() - startTime;
-  const uint32_t threshold =
-    NS_IsMainThread() ? Telemetry::kSlowSQLThresholdForMainThread
-                      : Telemetry::kSlowSQLThresholdForHelperThreads;
-  if (duration.ToMilliseconds() >= threshold) {
-    nsDependentCString statementString(aSqlString);
-    Telemetry::RecordSlowSQLStatement(statementString, mTelemetryFilename,
-                                      duration.ToMilliseconds());
-  }
 
   return srv;
 }
@@ -1995,10 +1954,6 @@ Connection::EnableModule(const nsACString& aModuleName)
   return NS_ERROR_FAILURE;
 }
 
-// Implemented in TelemetryVFS.cpp
-already_AddRefed<QuotaObject>
-GetQuotaObjectForFile(sqlite3_file *pFile);
-
 NS_IMETHODIMP
 Connection::GetQuotaObjects(QuotaObject** aDatabaseQuotaObject,
                             QuotaObject** aJournalQuotaObject)
@@ -2019,8 +1974,6 @@ Connection::GetQuotaObjects(QuotaObject** aDatabaseQuotaObject,
     return convertResultCode(srv);
   }
 
-  RefPtr<QuotaObject> databaseQuotaObject = GetQuotaObjectForFile(file);
-
   srv = ::sqlite3_file_control(mDBConn,
                                nullptr,
                                SQLITE_FCNTL_JOURNAL_POINTER,
@@ -2029,10 +1982,6 @@ Connection::GetQuotaObjects(QuotaObject** aDatabaseQuotaObject,
     return convertResultCode(srv);
   }
 
-  RefPtr<QuotaObject> journalQuotaObject = GetQuotaObjectForFile(file);
-
-  databaseQuotaObject.forget(aDatabaseQuotaObject);
-  journalQuotaObject.forget(aJournalQuotaObject);
   return NS_OK;
 }
 
